9deb66e6f10810910a12c4bb62ad07508e4a9f08
SonoraDE <tobiastischer@outlook.de>
2024-03-22 10:30:47 +0100

    DetailTeam

--- src/pages/DetailTeams/DetailTeam.jsx
+++ src/pages/DetailTeams/DetailTeam.jsx
@@ -1,7 +1,7 @@
 import "./DetailTeam.css";
 const DetailTeam = () => {
   return (
-    <section className="container">
+    <section className="container container-padding">
           <h1 className="team-name montserrat">ARSENAL</h1>
         <div className="header-flex">
           <div className="team-infos">
@@ -32,12 +32,35 @@ const DetailTeam = () => {
       </div>
       <div className="desc">
         <div>
-        <h2 className="desc-title">DESCRIPTION</h2>
+          <h2 className="desc-title montserrat">DESCRIPTION</h2>
         </div>
         <div className="desc-text">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis at blanditiis illum nisi! Dolor illo quas, eos fugiat voluptatum similique consectetur impedit voluptas quo molestias corporis suscipit ipsa quasi?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illo! Iure autem quidem atque tempora, voluptates aut dolorum asperiores nihil voluptate a unde maiores nulla quis laborum, neque sapiente eius.</p>
+          <p className="opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis at blanditiis illum nisi! Dolor illo quas, eos fugiat voluptatum similique consectetur impedit voluptas quo molestias corporis suscipit ipsa quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis at blanditiis illum nisi! Dolor illo quas, eos fugiat voluptatum similique consectetur impedit voluptas quo molestias corporis suscipit ipsa quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis at blanditiis illum nisi! Dolor illo quas, eos fugiat voluptatum similique consectetur impedit voluptas quo molestias corporis suscipit ipsa quasi?</p>
+          <p className="opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illo! Iure autem quidem atque tempora, voluptates aut dolorum asperiores nihil voluptate a unde maiores nulla quis laborum, neque sapiente eius.Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis at blanditiis illum nisi! Dolor illo quas, eos fugiat voluptatum similique consectetur impedit voluptas quo molestias corporis suscipit ipsa quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis at blanditiis illum nisi! Dolor illo quas, eos fugiat voluptatum similique consectetur impedit voluptas quo molestias corporis suscipit ipsa quasi?</p>
         </div>
+      </div>
+      <div className="logo-container">
+        <img className="team-logo" src="https://upload.wikimedia.org/wikipedia/hif/8/82/Arsenal_FC.png" alt="CLUB LOGO" />
+      </div>
+      <h3 className="stadiumtitle opensans">STADIUM</h3>
+      <section className="bottompage">
+        <div className="stadium opensans">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius repellat esse nostrum eum obcaecati, illo totam numquam maiores debitis. Hic, voluptas dolore explicabo quidem quia repellat beatae ab voluptatibus.loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius repellat esse nostrum eum obcaecati, illo totam numquam maiores debitis. Hic, voluptas dolore explicabo quidem quia repellat beatae ab voluptatibus.lorem</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius repellat esse nostrum eum obcaecati, illo totam numquam maiores debitis. Hic, voluptas dolore explicabo quidem quia repellat beatae ab voluptatibus.loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius repellat esse nostrum eum obcaecati, illo totam numquam maiores debitis. Hic, voluptas dolore explicabo quidem quia repellat beatae ab voluptatibus.lorem</p>
+        </div>
+        <div className="info1 opensans">
+          <p className="info1-title">EMIRATES STADIUM</p>
+          <p className="info1-info">Home</p>
+          <p className="info1-title">60338</p>
+          <p className="info1-info">Capacity</p>
+        </div>
+      </section>
+      <div className="socials opensans">
+        <a className="opensans" href="#">WEBSITE</a>
+        <a className="opensans" href="#">FACEBOOK</a>
+        <a className="opensans" href="#">TWITTER</a>
+        <a className="opensans" href="#">INSTAGRAM</a>
+        <a className="opensans" href="#">YOUTUBE</a>
       </div>
     </section>
   )
