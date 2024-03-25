33e1da41d81889b99dce258003a62703755ff070
SonoraDE <tobiastischer@outlook.de>
2024-03-25 15:22:51 +0100

    Detail Fertig

--- src/pages/DetailPlayer/DetailPlayer.jsx
+++ src/pages/DetailPlayer/DetailPlayer.jsx
@@ -1,5 +1,146 @@
+import "./DetailPlayer.css";
+import { useContext, useEffect, useState } from "react";
+import { AllPlayersContext } from "../../context/context";
+import { useParams } from "react-router-dom";
+
 const DetailPlayer = () => {
-  return <></>;
+  const params = useParams();
+  console.log("Params aus der URL:", params);
+  const { id } = params;
+  
+  const { players, setPlayers } = useContext(AllPlayersContext);
+  const [playerDetail, setPlayerDetail] = useState({});
+
+  useEffect(() => {
+    if (id) {
+        fetch(`https://www.thesportsdb.com/api/v1/json/60130162/searchplayers.php?p=${id}`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return res.json();
+            })
+            .then((playersData) => {
+                if (playersData && playersData.player) {
+                    setPlayers(playersData.player);
+                } else {
+                    console.log("Keine Spieler gefunden");
+                }
+            })
+            .catch((err) => {
+                console.log("Fehler beim Abrufen der Daten:", err);
+            });
+    }
+}, [id, setPlayers]);
+
+
+  console.log(players);
+
+  return (
+    <section className="container container-padding">
+        {players && players.length > 0 ? (
+            <>
+                <h1 className="player-name montserrat">{players[0]?.strPlayer}</h1>
+                <div className="header-flex">
+                    <div className="player-infos">
+                        {players[0]?.strNationality && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strNationality}</p>
+                          <p className="detail-text opensans">Nationality</p>
+                        </>
+                        }
+                        {players[0]?.strGender && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strGender}</p>
+                          <p className="detail-text opensans">Gender</p>
+                        </>
+                        }
+                        {players[0]?.strBirthLocation && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strBirthLocation}</p>
+                          <p className="detail-text opensans">Birth Location</p>
+                        </>
+                        }
+                        {players[0]?.dateBorn && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.dateBorn}</p>
+                          <p className="detail-text opensans">Date of Birth</p>
+                        </>
+                        }
+                        {players[0]?.strSport && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strSport}</p>
+                          <p className="detail-text opensans">Sport</p>
+                        </>
+                        }
+                        {players[0]?.strPosition && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strPosition}</p>
+                          <p className="detail-text opensans">Position</p>
+                        </>
+                        }
+                        {players[0]?.strStatus && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strStatus}</p>
+                          <p className="detail-text opensans">Status</p>
+                        </>
+                        }
+                        {players[0]?.strHeight && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strHeight}</p>
+                          <p className="detail-text opensans">Height</p>
+                        </>
+                        }
+                        {players[0]?.strWeight && 
+                        <>
+                          <p className="detail-headline montserrat">{players[0]?.strWeight}</p>
+                          <p className="detail-text opensans">Weight</p>
+                        </>
+                        }
+                    </div>
+                    <div className="player-img">
+                        <img src={players[0]?.strThumb} alt="Image" />
+                        {players[0]?.strDescriptionEN && 
+                        <>
+                          <h2 className="player-desc opensans">PLAYER DESCRIPTION</h2>
+                          <p className="player-details montserrat">{players[0]?.strDescriptionEN}</p>
+                        </>
+                        }
+                        <div className="player-socials opensans">
+                          {players[0]?.strWebsite &&
+                            <a className="opensans" target="_blank" href={"https://" + players[0]?.strWebsite}>
+                                WEBSITE
+                            </a>
+                          }
+                          {players[0]?.strFacebook &&
+                            <a className="opensans" target="_blank" href={"https://" + players[0]?.strFacebook}>
+                                FACEBOOK
+                            </a>
+                          }
+                          {players[0]?.strTwitter &&
+                            <a className="opensans" target="_blank" href={"https://" + players[0]?.strTwitter}>
+                                TWITTER
+                            </a>
+                          }
+                          {players[0]?.strInstagram &&
+                            <a className="opensans" target="_blank" href={"https://" + players[0]?.strInstagram}>
+                                INSTAGRAM
+                            </a>
+                          }
+                          {players[0]?.strYoutube &&
+                            <a className="opensans" target="_blank" href={"https://" + players[0]?.strYoutube}>
+                                YOUTUBE
+                            </a>
+                          }
+                      </div>
+                    </div>
+                </div>
+            </>
+        ) : (
+            <p>Keine Spieler gefunden</p>
+        )}
+    </section>
+);
 };
 
 export default DetailPlayer;
--- src/pages/DetailTeams/DetailTeam.jsx
+++ src/pages/DetailTeams/DetailTeam.jsx
@@ -27,78 +27,141 @@ const DetailTeam = () => {
 
   console.log(teams);
 
-    return (
-        <section className="container container-padding">
-            <h1 className="team-name montserrat">{teams[0].strTeam}</h1>
-            <div className="header-flex">
-                <div className="team-infos">
-                    <p className="detail-headline montserrat">{teams[0].strCountry}</p>
-                    <p className="detail-text opensans">Country</p>
-                    <p className="detail-headline montserrat">{teams[0].strStadiumLocation}</p>
-                    <p className="detail-text opensans">Location</p>
-                    <p className="detail-headline montserrat">{teams[0].intFormedYear}</p>
-                    <p className="detail-text opensans">Established</p>
-                    <p className="detail-headline montserrat">{teams[0].strSport}</p>
-                    <p className="detail-text opensans">Sport</p>
-                </div>
-                <div className="team-img">
-                    <img src="https://cdn.discordapp.com/attachments/1176890609542385764/1220380975985332265/w1anwa1588432105_1.png?ex=660ebb5a&is=65fc465a&hm=42d262ae8c46d99b03b2daa3fc9870490157dce9c5604549accbdcb697c89fbd&" alt="Image" />
-                </div>
+  return (
+    <section className="container container-padding">
+        {teams?.[0]?.strTeam && 
+            <>
+                <h1 className="team-name montserrat">{teams?.[0]?.strTeam || ''}</h1>
+            </>
+        }
+        <div className="header-flex">
+            <div className="team-infos">
+                {teams?.[0]?.strCountry && 
+                    <>
+                        <p className="detail-headline montserrat">{teams?.[0]?.strCountry}</p>
+                        <p className="detail-text opensans">Country</p>
+                    </>
+                }
+                {teams?.[0]?.strStadiumLocation && 
+                    <>
+                        <p className="detail-headline montserrat">{teams?.[0]?.strStadiumLocation}</p>
+                        <p className="detail-text opensans">Location</p>
+                    </>
+                }
+                {teams?.[0]?.intFormedYear && 
+                    <>
+                        <p className="detail-headline montserrat">{teams?.[0]?.intFormedYear}</p>
+                        <p className="detail-text opensans">Established</p>
+                    </>
+                }
+                {teams?.[0]?.strSport && 
+                    <>
+                        <p className="detail-headline montserrat">{teams?.[0]?.strSport}</p>
+                        <p className="detail-text opensans">Sport</p>
+                    </>
+                }
             </div>
-            <div className="comp opensans">
-                <div>
-                    <p className="detail-text">COMPETITION</p>
-                </div>
-                <div>
-                    <p className="detail-comp-text">{teams[0].strLeague}</p> <br />
-                    <p className="detail-comp-text">{teams[0].strLeague2}</p> <br />
-                    <p className="detail-comp-text">{teams[0].strLeague3}</p> <br />
-                    <p className="detail-comp-text">{teams[0].strLeague4}</p> <br />
-                    <p className="detail-comp-text">{teams[0].strLeague5}</p> <br />
-                </div>
+            <div className="team-img">
+                <img src="https://cdn.discordapp.com/attachments/1176890609542385764/1220380975985332265/w1anwa1588432105_1.png?ex=660ebb5a&is=65fc465a&hm=42d262ae8c46d99b03b2daa3fc9870490157dce9c5604549accbdcb697c89fbd&" alt="Image" />
             </div>
-            <div className="desc">
-                <div>
-                    <h2 className="desc-title montserrat">DESCRIPTION</h2>
-                </div>
-                <div className="desc-text">
-                    <p className="opensans">{teams[0].strDescriptionEN}</p>
-                </div>
+        </div>
+        <div className="comp opensans">
+            <div>
+                <p className="detail-text">COMPETITION</p>
             </div>
-            <div className="logo-container">
-                <img className="team-logo" src={teams[0].strTeamBadge} alt="CLUB LOGO" />
+            <div>
+                {teams?.[0]?.strLeague && 
+                    <>
+                        <p className="detail-comp-text">{teams?.[0]?.strLeague}</p> <br />
+                    </>
+                }    
+                {teams?.[0]?.strLeague2 && 
+                    <>
+                        <p className="detail-comp-text">{teams?.[0]?.strLeague2}</p> <br />
+                    </>
+                } {teams?.[0]?.strLeague3 && 
+                    <>
+                        <p className="detail-comp-text">{teams?.[0]?.strLeague3}</p> <br />
+                    </>
+                } {teams?.[0]?.strLeague4 && 
+                    <>
+                        <p className="detail-comp-text">{teams?.[0]?.strLeague4}</p> <br />
+                    </>
+                } {teams?.[0]?.strLeague5 && 
+                    <>
+                        <p className="detail-comp-text">{teams?.[0]?.strLeague5}</p> <br />
+                    </>
+                } 
             </div>
-            <h3 className="stadiumtitle opensans">STADIUM</h3>
-            <section className="bottompage">
+        </div>
+        {teams?.[0]?.strDescriptionEN && 
+            <>
+                <div className="desc">
+                    <div>
+                        <h2 className="desc-title montserrat">DESCRIPTION</h2>
+                    </div>
+                    <div className="desc-text">
+                        <p className="opensans">{teams?.[0]?.strDescriptionEN}</p>
+                    </div>
+                </div>
+            </>
+        }
+        <div className="logo-container">
+            <img className="team-logo" src={teams?.[0]?.strTeamBadge} alt="CLUB LOGO" />
+        </div>
+        <h3 className="stadiumtitle opensans">STADIUM</h3>
+        <section className="bottompage">
+        {teams?.[0]?.strStadiumDescription && 
+            <>
                 <div className="stadium opensans">
-                    <p>{teams[0].strStadiumDescription}</p>
+                    <p>{teams?.[0]?.strStadiumDescription}</p>
                 </div>
-                <div className="info1 opensans">
-                    <p className="info1-title">{teams[0].strStadium}</p>
-                    <p className="info1-info">Home</p>
-                    <p className="info1-title">{teams[0].intStadiumCapacity}</p>
-                    <p className="info1-info">Capacity</p>
-                </div>
-            </section>
-            <div className="socials opensans">
-                <a className="opensans" href={teams[0].strWebsite}>
-                    WEBSITE
-                </a>
-                <a className="opensans" href={teams[0].strFacebook}>
-                    FACEBOOK
-                </a>
-                <a className="opensans" href={teams[0].strTwitter}>
-                    TWITTER
-                </a>
-                <a className="opensans" href={teams[0].strInstagram}>
-                    INSTAGRAM
-                </a>
-                <a className="opensans" href={teams[0].strYoutube}>
-                    YOUTUBE
-                </a>
+            </>
+        }
+            <div className="info1 opensans">
+                {teams?.[0]?.strStadium && 
+                    <>
+                        <p className="info1-title">{teams?.[0]?.strStadium}</p>
+                        <p className="info1-info">Home</p>
+                    </>
+                }
+                {teams?.[0]?.intStadiumCapacity && 
+                    <>
+                        <p className="info1-title">{teams?.[0]?.intStadiumCapacity}</p>
+                        <p className="info1-info">Capacity</p>
+                    </>
+                }
             </div>
         </section>
-    );
+        <div className="socials opensans">
+            {teams?.[0]?.strWebsite &&
+            <a className="opensans" target="_blank" href={"https://" + teams?.[0]?.strWebsite}>
+                WEBSITE
+            </a>
+            }
+            {teams?.[0]?.strFacebook &&
+            <a className="opensans" target="_blank" href={"https://" + teams?.[0]?.strFacebook}>
+                FACEBOOK
+            </a>
+            }
+            {teams?.[0]?.strTwitter &&
+            <a className="opensans" target="_blank" href={"https://" + teams?.[0]?.strTwitter}>
+                TWITTER
+            </a>
+            }
+            {teams?.[0]?.strInstagram &&
+            <a className="opensans" target="_blank" href={"https://" + teams?.[0]?.strInstagram}>
+                INSTAGRAM
+            </a>
+            }
+            {teams?.[0]?.strYoutube &&
+            <a className="opensans" target="_blank" href={"https://" + teams?.[0]?.strYoutube}>
+                YOUTUBE
+            </a>
+            }
+        </div>
+    </section>
+);
 };
 
 export default DetailTeam;
