1155c57ba8efb758d0e231a5292c9e692e3c1b0e
MichaelGee76 <gottungmichael@gmail.com>
2024-03-26 13:55:41 +0100

    loading screen, footer updated

--- src/componenten/Footer/Footer.jsx
+++ src/componenten/Footer/Footer.jsx
@@ -2,18 +2,22 @@ import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
-  let year = new Date().getFullYear();
-  return (
-    <footer>
-      <p>&copy; {year} Die Erdnussbutterhörnchen</p>
-      <div className="footerLinkBox">
-        <Link to={"https://github.com/fredwardp"}>Fred vom Jupiter</Link>
-        <Link to={"https://github.com/SonoraDE"}>Tobel Hobel</Link>
-        <Link to={"https://github.com/Hkndevit"}>Hakan Kanka</Link>
-        <Link to={"https://github.com/MichaelGee76"}>Mike the Mechanic</Link>
-      </div>
-    </footer>
-  );
+    let year = new Date().getFullYear();
+    return (
+        <footer>
+            <p className="footerParagraph">&copy; {year} Sports Database</p>
+            <div className="footerLinkBox">
+                <div className="footerInnerBox">
+                    <Link to={"https://github.com/fredwardp"}>Fred vom Jupiter</Link>
+                    <Link to={"https://github.com/SonoraDE"}>Tobel Hobel</Link>
+                </div>
+                <div className="footerInnerBox">
+                    <Link to={"https://github.com/Hkndevit"}>Hakan Kanka</Link>
+                    <Link to={"https://github.com/MichaelGee76"}>Mike the Mechanic</Link>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/componenten/LoadingScreen/LoadingScreen.jsx
+++ src/componenten/LoadingScreen/LoadingScreen.jsx
@@ -4,22 +4,23 @@ import FetchData from "../FetchData/FetchData";
 import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
-  const { loading, setLoading } = useContext(LoadingContext);
+    const { loading, setLoading } = useContext(LoadingContext);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(true);
-    }, 2000);
-  }, []);
-  return (
-    <section>
-      <FetchData />
-      <div className="loading-anim">
-        <img src="../../../public/img/anim.gif" alt="Loading nimation" />
-        <h1>DIE ERDNUSSBUTTER BANDE</h1>
-      </div>
-    </section>
-  );
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(true);
+        }, 2000);
+    }, []);
+    return (
+        <section>
+            <FetchData />
+            <div className="loading-anim">
+                <img src="../../../public/img/anim.gif" alt="Loading nimation" />
+                <h1>Sports Database</h1>
+                <h3>The only sports resource you need</h3>
+            </div>
+        </section>
+    );
 };
 
 export default LoadingScreen;
